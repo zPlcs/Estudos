4f9d7223ea3111ee68859701130017d45d3fd119
Patrik Luiz <111705938+zPlcs@users.noreply.github.com>
2025-03-17 18:56:27 -0300

    Atualizar o useEffect.js

--- Linguagens/React Native/Concepts/Hooks/useEffect.js
+++ Linguagens/React Native/Concepts/Hooks/useEffect.js
@@ -1,1 +1,23 @@
-//Começar a fazer saporra no ônibus e terminar em casa.
+import React, { useEffect, useState } from 'react'
+import { View, Text } from 'react-native'
+
+export default function App(){
+const [num,setNum] = useState(0);
+
+const inc = () => {
+    return setNum((prevNum) => prevNum+1);
+}
+
+useEffect(() => {
+    setInterval(() => {
+        setNum((prevNum) => prevNum+1);
+   },1000);
+},[]);
+
+clearInterval();
+    return(
+        <View>
+            <Text>{num}</Text>
+        </View>
+    );
+}
