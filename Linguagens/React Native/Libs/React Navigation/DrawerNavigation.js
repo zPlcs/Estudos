9338f8018a5eb672ddb49644e503c07be207de74
Patrik Luiz <patrikluiz2004@hotmail.com>
2025-03-18 12:52:23 -0300

    Update DrawerNavigation.js

--- Linguagens/React Native/Libs/React Navigation/DrawerNavigation.js
+++ Linguagens/React Native/Libs/React Navigation/DrawerNavigation.js
@@ -1,45 +1,44 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const Drawer = createDrawerNavigation();
+const Drawer = createDrawerNavigator();
 
-function Option1(){
-    return(
+function Option1() {
+    return (
         <View>
             <Text>Opção 1</Text>
         </View>
     );
 }
 
-function Option2(){
-    return(
+function Option2() {
+    return (
         <View>
             <Text>Opção 2</Text>
         </View>
     );
 }
 
-function Option3(){
-    return(
+function Option3() {
+    return (
         <View>
             <Text>Opção 3</Text>
         </View>
     );
 }
 
-function RootStack(){
-    return(
+function RootStack() {
+    return (
         <Drawer.Navigator>
-           <Drawer.Screen name='Opção 1' component={Option1} />
-        </Drawer.Navigator>
-        <Drawer.Screen name='Opção 2' component={Option2} />
-        <Drawer.Screen name='Opção 3' component={Option3} />
-            </Drawer.Navigator>
+            <Drawer.Screen name='Opção 1' component={Option1} />
+            <Drawer.Screen name='Opção 2' component={Option2} />
+            <Drawer.Screen name='Opção 3' component={Option3} />
+        </Drawer.Navigator >
     );
 }
 
-export default function App(){
-    return(
+export default function App() {
+    return (
         <NavigationContainer>
             <RootStack />
         </NavigationContainer>
