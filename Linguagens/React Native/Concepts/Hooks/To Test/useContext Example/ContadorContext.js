9445cd5b37521d6706445e78e6260ad89c7591d3
Patrik Luiz <111705938+zPlcs@users.noreply.github.com>
2025-03-24 09:31:40 -0300

    Atualizar o ContadorContext.js

---
+++ Linguagens/React Native/Concepts/Hooks/To Test/useContext Example/ContadorContext.js
@@ -0,0 +1,11 @@
+import React, { useState, createContext } from 'react'
+
+export const ContadorContext = createContext();
+
+export function ContextProvider({children}){
+const [num, setNum] = useState(0)
+
+    <ContadorContext.Provider>
+        {children}
+    </ContadorContext.Provider>
+}
