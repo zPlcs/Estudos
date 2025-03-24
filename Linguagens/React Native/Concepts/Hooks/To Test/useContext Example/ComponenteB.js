4918fd416c4ab7494d3fd64c03e8fe20508e6af5
Patrik Luiz <111705938+zPlcs@users.noreply.github.com>
2025-03-24 09:31:22 -0300

    Atualizar o ComponenteB.js

---
+++ Linguagens/React Native/Concepts/Hooks/To Test/useContext Example/ComponenteB.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+import {View, Button, Text} from 'react-native'
+
+import ContadorContext from './ContadorContext'
+
+export default function ComponenteB(){
+    const {num, setNum} = useContext(ContadorContext);
+    return (
+        <View style={{ padding: 20, borderColor: 'black', borderWidth: 2}}>
+            <Text>Componente B</Text>
+            <Text>{num}</Text>
+            <Button title="Decrement" onPress={(PrevNum) => setNum(PrevNum - 1)}
+        </View>
+    );
+}
